7ab7d3ce48970289e70f8906808321722cfd50bc
gvnikhil <gvnikhil21@gmail.com>
2020-09-24 14:18:04 +0530

    Added: Check Employee Part-Time Functionality Added

--- Employee.java
+++ Employee.java
@@ -3,8 +3,10 @@ package employee;
 public class Employee {
 	//Constants
 	public static final int IS_PRESENT=1;
+	public static final int IS_PART_TIME=2;
 	public static final int WAGE_PER_HOUR=20;
 	public static final int FULL_DAY_HOUR=8;
+	public static final int PART_TIME_HOUR=4;
 
 	public static void main(String[] args) {
 		//Variables
@@ -14,10 +16,15 @@ public class Employee {
 		//Welcome statement
 		System.out.println("Welcome to Employee Wage Computation Program!");
 
-		//Check Employee Present or Absent
-		double empCheck=Math.floor(Math.random()*10)%2;
+		//Check Employee Present or Absent 
+		//Check Employee Full-Time or Part-Time
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		if(empCheck==IS_PRESENT) 
-			System.out.println("Employee is Present!");
+			System.out.println("Employee is Full-Time and Present!");
+		else if(empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Part-Time and Present!");
+			noOfHour = PART_TIME_HOUR;
+		}
 		else 
 			System.out.println("Employee is Absent!");
 		
