b0ae08cf494e03200948e35de5b32f20a2072306
gvnikhil <gvnikhil21@gmail.com>
2020-09-24 13:35:01 +0530

    Added: Welcome Message Added

---
+++ Employee.java
@@ -0,0 +1,10 @@
+package employee;
+
+public class Employee {
+	public static void main(String[] args) {
+		
+		//Welcome statement
+		System.out.println("Welcome to Employee Wage Computation Program!");
+	}
+}
+
