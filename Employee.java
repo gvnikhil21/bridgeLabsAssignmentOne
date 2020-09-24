3010a415eb63f78076e2732dd4b74d0081adcb6d
gvnikhil <gvnikhil21@gmail.com>
2020-09-24 14:07:19 +0530

    Added: Calculate Daily Employee Wage Functionality Added

--- Employee.java
+++ Employee.java
@@ -1,7 +1,15 @@
 package employee;
 
 public class Employee {
+	//Constants
+	public static final int IS_PRESENT=1;
+	public static final int WAGE_PER_HOUR=20;
+	public static final int FULL_DAY_HOUR=8;
+
 	public static void main(String[] args) {
+		//Variables
+		int noOfHour=0;
+		int dailyWage=0;		
 		
 		//Welcome statement
 		System.out.println("Welcome to Employee Wage Computation Program!");
@@ -12,6 +20,10 @@ public class Employee {
 			System.out.println("Employee is Present!");
 		else 
 			System.out.println("Employee is Absent!");
+		
+		//Calculate Daily Employee Wage
+		dailyWage = noOfHour * WAGE_PER_HOUR;
+		System.out.println("Daily Employee Wage is : "+ dailyWage);
 	}
 }
 
