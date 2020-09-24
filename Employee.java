2ea47f85242a2318a04dd8aeff28bb14dad77592
gvnikhil <gvnikhil21@gmail.com>
2020-09-24 13:56:01 +0530

    Added: Check Employee is Present of Absent Functionality Added

--- Employee.java
+++ Employee.java
@@ -5,6 +5,13 @@ public class Employee {
 		
 		//Welcome statement
 		System.out.println("Welcome to Employee Wage Computation Program!");
+
+		//Check Employee Present or Absent
+		double empCheck=Math.floor(Math.random()*10)%2;
+		if(empCheck==IS_PRESENT) 
+			System.out.println("Employee is Present!");
+		else 
+			System.out.println("Employee is Absent!");
 	}
 }
 
