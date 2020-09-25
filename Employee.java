82a15ce8f14b884e31c560a1de083c2135be5d68
gvnikhil21 <69467502+gvnikhil21@users.noreply.github.com>
2020-09-25 23:32:33 +0530

    Refactored: Code refactored to write class method for calculating employee wage

--- Employee.java
+++ Employee.java
@@ -1,55 +1,67 @@
 package employee;
 
 public class Employee {
-	//Constants
-	public static final int IS_PRESENT=1;
-	public static final int IS_PART_TIME=2;
-	public static final int WAGE_PER_HOUR=20;
-	public static final int FULL_DAY_HOUR=8;
-	public static final int PART_TIME_HOUR=4;
-	public static final int WORKING_DAYS_PER_MONTH=20;
-	public static final int TOTAL_WORKING_HOURS=100;
+	// Constants
+	public static final int IS_PRESENT = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int WAGE_PER_HOUR = 20;
+	public static final int FULL_DAY_HOUR = 8;
+	public static final int PART_TIME_HOUR = 4;
+	public static final int WORKING_DAYS_PER_MONTH = 20;
+	public static final int TOTAL_WORKING_HOURS = 100;
 
-	public static void main(String[] args) {
-		//Variables
-		int noOfHour=0;
-		int totalWage=0;
-		int noOfDays=0;
-		//Welcome statement
-		System.out.println("Welcome to Employee Wage Computation Program!");
+	// Variables
+	private int totalWage = 0;
+	private int noOfHours = 0;
+	private int noOfDays = 0;
+	private int empHour = 0;
+	private String empType = "";
 
-		System.out.println("EmpType\t\tHours\tDays\tWage");
-		//Applying condition for calculating wage of employee till 100Hours/20Days 
-		double empCheck = Math.floor(Math.random() * 10) % 3;
-		switch((int)empCheck) {
+	// calculates employee hours till 100Hours/20Days whichever earlier is reached
+	public int calculateEmpHours() {
+		while (noOfHours < TOTAL_WORKING_HOURS && noOfDays < WORKING_DAYS_PER_MONTH) {
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			noOfDays++;
+			switch ((int) empCheck) {
 			case IS_PRESENT:
-				while(noOfHour <= TOTAL_WORKING_HOURS && noOfDays<WORKING_DAYS_PER_MONTH){
-				noOfHour += FULL_DAY_HOUR;
-				noOfDays++;
-			}
-				if(noOfHour > TOTAL_WORKING_HOURS)
-				noOfHour=TOTAL_WORKING_HOURS;
-				//Calculate Total Employee Wage
-				totalWage = noOfHour * WAGE_PER_HOUR;
-				System.out.println("FullTime\t"+noOfHour+"\t"+noOfDays+"\t"+totalWage);
+				empType = "Full-Time";
+				empHour = FULL_DAY_HOUR;
 				break;
 			case IS_PART_TIME:
-				while(noOfHour <= TOTAL_WORKING_HOURS && noOfDays<WORKING_DAYS_PER_MONTH){
-				noOfHour += PART_TIME_HOUR;
-				noOfDays++;
-			}
-				if(noOfHour > TOTAL_WORKING_HOURS)
-				noOfHour=TOTAL_WORKING_HOURS;
-				//Calculate Total Employee Wage
-				totalWage = noOfHour * WAGE_PER_HOUR;
-				System.out.println("PartTime\t"+noOfHour+"\t"+noOfDays+"\t"+totalWage);
+				empType = "Part-Time";
+				empHour = PART_TIME_HOUR;
 				break;
 			default:
-				//Calculate Total Employee Wage
-				totalWage = noOfHour * WAGE_PER_HOUR;
-				System.out.println("Absent\t\t"+noOfHour+"\t"+noOfDays+"\t"+totalWage);
+				empType = "Absent\t";
+				empHour = 0;
 				break;
+			}
+			noOfHours += empHour;
+			if (noOfHours > TOTAL_WORKING_HOURS)
+				noOfHours = TOTAL_WORKING_HOURS;
+			System.out.println(empType + "\t" + noOfHours + "\t" + noOfDays);
 		}
+		return noOfHours;
+	}
+
+	// calculates total wage of employee
+	public void calculateEmpWage(int totalHour) {
+		totalWage = totalHour * WAGE_PER_HOUR;
+		System.out.println("-----------------------------");
+		System.out.println("Total Employee Wage is : " + totalWage);
+	}
+
+	// main method
+	public static void main(String[] args) {
+		// Welcome statement
+		System.out.println("Welcome to Employee Wage Computation Program!");
+		System.out.println("-----------------------------");
+		System.out.println("EmpType\t\tHours\tDays");
+		System.out.println("-----------------------------");
+
+		// creating object employee of Employee class
+		Employee employee = new Employee();
+		int totalHour = employee.calculateEmpHours();
+		employee.calculateEmpWage(totalHour);
 	}
 }
-
